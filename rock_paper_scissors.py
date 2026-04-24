32a714a5b9e28504fa49144922e6c1d1fd508179
Trace <therealtraceofhope@gmail.com>
2026-04-24 14:35:49 -0600

    Initial upload of rock_paper_scissors.py

---
+++ rock_paper_scissors.py
@@ -0,0 +1,75 @@
+import random
+
+
+print("THANK YOU FOR PLAYING OUR ROCK PAPER SCISSORS GAME")
+print("YOUR CHOICES ARE AS FOLLOWS - ")
+rock_selection = """
+ROCK:
+      _______
+# ---'   ____)
+#       (_____)
+#       (_____)
+#       (____)
+# ---.__(___)"""
+
+
+paper_selection = """
+PAPER:                         
+      _______
+# ---'    ____)____
+#            ______)
+#           _______)
+#          _______)
+# ---.__________)"""
+
+
+scissors_selection = """
+SCISSORS:
+      _______
+# ---'   ____)____
+#           ______)
+#        __________)
+#       (____)
+# ---.__(___)"""
+
+choice = input(f"Make your choice! Rock, Paper or Scizzors\n").capitalize
+
+if choice == "ROCK":
+    user_selection = rock_selection
+elif choice == "PAPER":
+    user_selection = paper_selection
+else:
+    user_selection = scissors_selection
+print(f"User Selection: {user_selection}")
+#I already added the asci art
+#I already created the user prompt along with the 
+# the computer will randomly pull from a list that includes Rock,Paper,Scissors.
+game_options = [rock_selection, paper_selection, scissors_selection]
+computer_selection = random.choice(game_options)
+print(f"Computer Selection: {computer_selection}")
+
+# help(random)
+match_result = ""
+if computer_selection == rock_selection and user_selection == rock_selection:
+    match_result = "MATCH RESULT: TIE"
+elif computer_selection == rock_selection and user_selection == paper_selection:
+    match_result = "MATCH RESULT: USER WINS!"
+elif computer_selection == rock_selection and user_selection == scissors_selection:
+    match_result = "MATCH RESULT: COMPUTER WINS!"
+elif computer_selection == scissors_selection and user_selection == rock_selection:
+    match_result = "MATCH RESULT: USER WINS!"
+elif computer_selection == scissors_selection and user_selection == scissors_selection:
+    match_result = "MATCH RESULT: TIE"
+elif computer_selection == scissors_selection and user_selection == paper_selection:
+    match_result = "MATCH RESULT: COMPUTER WINS!"
+elif computer_selection == paper_selection and user_selection == rock_selection:
+    match_result = "MATCH RESULT: COMPUTER WINS!"
+elif computer_selection == paper_selection and user_selection == paper_selection:
+    match_result = "MATCH RESULT: TIE"
+elif computer_selection == paper_selection and user_selection == scissors_selection:
+    match_result = "MATCH RESULT: USER WINS!"
+
+print(match_result)
+
+
+
